e81627abb15fa7fe48c8995e7d98eb81a99719fb
DarkTron72 <yannik.rauter@hotmail.com>
2019-01-17 08:53:42 +0100

    Changed auto stop

--- Raspi SD2 (ROS)/lidar_controller.py
+++ Raspi SD2 (ROS)/lidar_controller.py
@@ -5,14 +5,18 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
 
+# Global publisher
+publisher = None
+
+#variables
 LOW_DISTANCE_VALUE = 1.0
-LOW_DISTANCE_COUNT = 2
-forward_distance = 0.0
-forward_clear = False
+LOW_DISTANCE_COUNT_MAX = 2
+#forward_distance = 0.0
+is_forward_clear = False
 
 def lidar_data_handler(data):
-	global forward_distance
-	global forward_clear
+	#global forward_distance
+	global is_forward_clear
 
 	# Store ranges
 	ranges = data.ranges
@@ -20,38 +24,47 @@ def lidar_data_handler(data):
 	values = [None] * 50
 
 	# Fill chosen values array
-	for i in range(0, 25):
-		values[i] = ranges[i]
-	for i in range(25, 50):
-		values[i] = ranges[310 + i]
+	for i in range(0, 50):
+		values[i] = ranges[155 + i]
+
+	# Check if forward is clear and publish its state
+	currentLowDistCount = 0
+	for i in range(0, 50):
+		if (values[i] != float('Inf')) and (values[i] <= LOW_DISTANCE_VALUE):
+			currentLowDistCount += 1
+	if currentLowDistCount >= LOW_DISTANCE_COUNT_MAX:
+		is_forward_clear = False
+	else:
+		is_forward_clear = True
+
+	publisher.publish(is_forward_clear)
 
 	# Count how many inf and lowDist
-	valueSum = 0.0;
-	infCount = 0
-	lowDistCount = 0
-	for i in range(0, 50):
-		if values[i] == float('Inf'):
-			infCount += 1
-		else:
-			valueSum += values[i]
-			if values[i] <= LOW_DISTANCE_VALUE:
-				lowDistCount += 1
-	if lowDistCount >= LOW_DISTANCE_COUNT:
-		forward_clear = False
-	else:
-		forward_clear = True
-	forward_distance = valueSum / (50 - infCount)
+	# valueSum = 0.0;
+	# infCount = 0
+	# lowDistCount = 0
+	# for i in range(0, 50):
+	# 	if values[i] == float('Inf'):
+	# 		infCount += 1
+	# 	else:
+	# 		valueSum += values[i]
+	# 		if values[i] <= LOW_DISTANCE_VALUE:
+	# 			lowDistCount += 1
+	# if lowDistCount >= LOW_DISTANCE_COUNT_MAX:
+	# 	is_forward_clear = False
+	# else:
+	# 	is_forward_clear = True
+	# forward_distance = valueSum / (50 - infCount)
 	#print("infCount", infCount, "lowDistCount", lowDistCount, "forward_distance", forward_distance, "forward_clear", forward_clear)
 
 def lidar_data_receiver_setup():
+	global publisher
 	publisher = rospy.Publisher('lucy/lidar_forward_clear', Bool, queue_size=10)
 	rospy.init_node('lidar_controller', anonymous=True)
 	rospy.Subscriber('/scan', LaserScan, lidar_data_handler)
 
-	rate = rospy.Rate(10)
-	while not rospy.is_shutdown():
-		publisher.publish(forward_clear)
-		rate.sleep()
+	rospy.spin()
+		
 	print("Stopped lidar_controller")
 
 if __name__ == '__main__':
